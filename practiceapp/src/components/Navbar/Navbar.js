fc0fe690d45e1f56d044a9ee7c3996e9a69dd288
Isaias Del Mundo <idelmundo68@gmail.com>
2020-08-08 22:58:22 -0700

    added component and skeleton of Navbar

---
+++ practiceapp/src/components/Navbar/Navbar.js
@@ -0,0 +1,20 @@
+// clicky game gets route to homepage
+import React from "react";
+import "./navbarStyle.css"
+
+function Navbar() {
+    return (
+      <div>
+        <h1>Hey guys! I'm a header!</h1>
+        <h2>Things I like!</h2>
+        <ul>
+          <li>Peas</li>
+          <li>Carrots</li>
+          <li>Love</li>
+        </ul>
+      </div>
+    );
+  }
+  
+  export default Navbar;
+  
