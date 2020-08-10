4848104aefa976735cfe9b4593ec5a4b67095c5a
Isaias Del Mundo <idelmundo68@gmail.com>
2020-08-09 20:33:51 -0700

    stuck at clickit

--- practiceapp/src/App.js
+++ practiceapp/src/App.js
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Wrapper from "./components/Wrapper/Wrapper";
 import Card from "./components/Card/Card";
+import Jumbotron from "./components/Jumbotron/Jumbotron";
+import familyGuy from "./familyguy.json"
 
-function App() {
-  return (
-    <div>
-      <Navbar/>
-      {/* <Header /> */}
-      {/* <Section /> */}
-    </div>
-  );
+// by extending the react component class counter inherits functionality from it.
+function App extends Component () {
+  // Setting initial state of the counter component
+  state= {
+   images,
+   score: 0,
+   highScore: 0,
+   clicked: []
+  };
+
+    clickIt = id => {
+      
+    }
+  
+  render() {
+    return (
+      <Wrapper>
+      <div>
+        <Navbar/>
+        <Jumbotron/>
+        <Card />
+      </div>
+      </Wrapper>
+      
+    );
+
+  }
+  
 }
 
 export default App;
