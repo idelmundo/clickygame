2dd09d04d1bc76122c321dcd97fb28f0f8e5c12f
Isaias Del Mundo <idelmundo68@gmail.com>
2020-08-11 22:06:03 -0700

    Updates

---
+++ precache-manifest.4fcac5e49ff016691315ea7529c80f8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edb3abfc6ab0f8dcdfcae18ade6cb844",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "0ef3f13a0a589a9f834a",
+    "url": "/clickygame/static/css/main.5f9a022f.chunk.css"
+  },
+  {
+    "revision": "21bef3b733df7c75525e",
+    "url": "/clickygame/static/js/2.16d59b1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clickygame/static/js/2.16d59b1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ef3f13a0a589a9f834a",
+    "url": "/clickygame/static/js/main.46dd7824.chunk.js"
+  },
+  {
+    "revision": "51fd1dc30586a0e4d488",
+    "url": "/clickygame/static/js/runtime-main.1dc1a56f.js"
+  }
+]);
